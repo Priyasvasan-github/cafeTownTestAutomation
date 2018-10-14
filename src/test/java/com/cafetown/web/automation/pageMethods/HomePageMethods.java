1f956f953537572d6f77ee93ccfa9e8466d60259
Sripriya Srinivasan <sripriyavasan@gmail.com>
2018-10-14 22:23:28 +0200

    Adding prop file to input data into execution

--- src/test/java/com/cafetown/web/automation/pageMethods/HomePageMethods.java
+++ src/test/java/com/cafetown/web/automation/pageMethods/HomePageMethods.java
@@ -4,11 +4,18 @@ import com.cafetown.web.automation.pageObjects.HomePage;
 import net.thucydides.core.annotations.Step;
 import org.yecht.Data;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by Sripriya srinivasan on 10/13/2018.
  */
 public class HomePageMethods {
     private HomePage homePage;
+    Properties prop = new Properties();
+    InputStream input = null;
 
     public void launchApplication(){
         homePage.open();
@@ -41,7 +48,7 @@ public class HomePageMethods {
     @Step
     public void loginUsingValidCredentials(){
         homePage.open();
-        login("Luke","Skywalker");
+        login(getPropValue("user.name"),getPropValue("user.password"));
     }
 
     /**
@@ -51,5 +58,20 @@ public class HomePageMethods {
     @Step
     public boolean isHomePageLoaded(){
         return homePage.isLoginButtonDisplayed();
+    }
+
+    /**
+     * This method returns the input values provided in config value
+     * @param propName
+     * @return
+     */
+    public String getPropValue(String propName){
+        try{
+            input = new FileInputStream("src/main/resources/config.properties");
+            prop.load(input);
+            return prop.getProperty(propName);
+        }catch (IOException exception){
+            return null;
+        }
     }
 }
